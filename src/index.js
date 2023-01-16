1de0689c84e82bc6a1f46cfc8d496f0efcb08a65
PsychoMensis <103057687+PsychoMensis@users.noreply.github.com>
2023-01-16 22:12:58 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = ''
+    n = n.toString().replace('-', '');
+    for (let i = n.length-1; i >= 0; i--) {
+        result = result + n[i]
+    }
+  return result
 }
+
